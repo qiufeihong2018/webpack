5cf1140fd3fd8e2525e9ffafc2d17a94193c31ef
qiufeihong <qiufeihong2018@126.com>
2019-04-04 10:52:53 +0800

    feat:add Development - watch mode - webpack-dev-server - webpack-dev-middle

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
 import _ from 'lodash'
-import './style.css'
-import qiao from './qiao.jpg'
-import data from './data.xml'
 
 function component() {
     let element = document.createElement('div')
     //lodash
     element.innerHTML = _.join(['hello', 'webpack'], ' ')
-    element.classList.add('hello')
-    element.classList.add('*')
-    //images
-    let myPic = new Image()
-    myPic.src = qiao
-    element.appendChild(myPic)
-    console.log(data)
     return element
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,25 @@
 const path = require('path')
-
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, 'dist')
+    mode: 'development',
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'
     },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader'
-                ]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    'file-loader'
-                ]
-            },
-            {
-                test: /\.(csv|tsv)$/,
-                use: [
-                    'csv-loader'
-                ]
-            },
-            {
-                test: /\.xml$/,
-                use: [
-                    'xml-loader'
-                ]
-            }
-        ]
+    devtool: "inline-source-map",
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: "dev"
+        })
+    ],
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: "/"
     }
 }
