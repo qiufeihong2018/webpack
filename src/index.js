b82dc765f2e599367a645192dc10dfdd4a1f0b40
qiufeihong <qiufeihong2018@126.com>
2019-04-04 10:20:47 +0800

    feat:add Asset Management - css - images - fonts - data

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import _ from 'lodash'
+import './style.css'
+import qiao from './qiao.jpg'
+import data from './data.xml'
+
 function component() {
     let element = document.createElement('div')
+    //lodash
     element.innerHTML = _.join(['hello', 'webpack'], ' ')
+    element.classList.add('hello')
+    element.classList.add('*')
+    //images
+    let myPic = new Image()
+    myPic.src = qiao
+    element.appendChild(myPic)
+    console.log(data)
     return element
 }
 
